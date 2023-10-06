d8344fb7111b2b1c2de6cb93b766613137e42f3b
QuangHuy <nguyenvanquanghuy051102@gmail.com>
2023-10-06 14:53:21 +0700

    landingpagecomit

---
+++ fe/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.scss'
+
+const Footer = () => {
+    return(
+        <footer>
+            <div className="container">
+                <p>Dự án thuộc bản quyền của TTN Company</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- fe/src/routes/index.js
+++ fe/src/routes/index.js
@@ -2,6 +2,7 @@ import {  createBrowserRouter } from "react-router-dom";
 import Login from "../components/Login.component";
 import Home from "../components/Home.component";
 import Header from "../components/Header/Header.component";
+import Landingpage from "../page/Landingpage.js";
 
 
 
@@ -11,14 +12,13 @@ export const appRoutes = createBrowserRouter([
         path: '',
         element: <Header />,
         children: [
-
             {
                 path: '/login',
                 element: <Login />
             },
             {
                 path: '/home',
-                element: <Home />
+                element: <Landingpage />
             }
         ]
     }

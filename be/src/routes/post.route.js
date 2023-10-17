81436e811009bd7e66b91e24fddba6d7f68eee9b
Nang Khanh <nangkhan311202@gmail.com>
2023-10-17 15:00:40 +0700

    add serch & filter api

--- be/src/routes/post.route.js
+++ be/src/routes/post.route.js
@@ -6,5 +6,7 @@ const {verifyToken}=require("../middlewares/verifyToken.middleware");
 router.post('/',verifyToken,validatePOST, PostController.createOne);
 router.put('/:id',verifyToken,validatePUT, PostController.updateOne);
 router.get('/:id',verifyToken,PostController.getDetail);
+router.get('/search/:searchParam', verifyToken, PostController.getSearchValue);
+router.post('/filter/', verifyToken, PostController.getFilterValue);
 
 module.exports = router;

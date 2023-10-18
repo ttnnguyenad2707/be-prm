8095cba0642eeab03d3b202bdeea57f863eb84b2
Nang Khanh <nangkhan311202@gmail.com>
2023-10-18 17:23:24 +0700

    fix bug of filterPost

--- be/src/controllers/post.controller.js
+++ be/src/controllers/post.controller.js
@@ -41,7 +41,7 @@ module.exports = {
         const { address, area, price, utils, currentPage} = req.body;
         try {
             const result = await PostService.getFilterValue({ address, area, price, utils, currentPage });
-            return result
+            return res.status(200).json({ message: "filter result", data: result });
         } catch (error) {
             return res.status(500).json({ error: error.message });
         }
